c979dd28df78a02180401e52eb4fd0a720ea8c2b
Larry Aasen <larryaasen@gmail.com>
2011-09-05 09:16:42 -0400

    Initial commit

---
+++ rails_appdorm/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAppdorm::Application.config.secret_token = '868238efaebb86dff2fbf95b04026f341ff3cc6564a8002beaf4f4a9236e560d31ae679f7a305455a842d00194a15c899bc6ae157ef496ea734e24c18448a023'
